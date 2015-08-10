8e1583426eea48dceff2c05269a26337db4efee6
Joe Abhishek <joeabhishek@gmail.com>
2015-08-10 01:19:19 -0400

    Update navbar of projects page.

--- Volar Template/js/scripts.js
+++ Volar Template/js/scripts.js
@@ -1244,3 +1244,38 @@
     }
     $('.svg-container').addClass('animate-svg');
 })(jQuery);
+
+var jump=function(e)
+{
+   if (e){
+       e.preventDefault();
+       var target = $(this).attr("href");
+   }else{
+       var target = location.hash;
+   }
+
+   $('html,body').animate(
+   {
+       scrollTop: $(target).offset().top
+   },2000,function()
+   {
+       location.hash = target;
+   });
+
+}
+
+$('html, body').hide();
+
+$(document).ready(function()
+{
+    $('a[href^=#]').bind("click", jump);
+
+    if (location.hash){
+        setTimeout(function(){
+            $('html, body').scrollTop(0).show();
+            jump();
+        }, 0);
+    }else{
+        $('html, body').show();
+    }
+});
